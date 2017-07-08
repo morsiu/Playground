29d2ada285f7595583e67acf0f75b530cbd98f3e
Łukasz Mrozek <morskk@gmail.com>
2017-07-08 17:46:48 +0200

    Added monad type class

---
+++ TypeClasses/Monad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TypeClasses
+{
+    public static class Monad
+    {
+        public interface IMonand<TInputMonad, TOutputMonad, TInput, TOutput, TApplicative, TFunctor>
+        {
+            TOutputMonad Return(TOutput output);
+
+            TOutputMonad Bind(Func<TInput, TOutputMonad> function, TInputMonad input);
+        }
+    }
+}
