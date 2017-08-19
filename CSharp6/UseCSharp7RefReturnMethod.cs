612da3835e1f8c84b0955efcae99daf1361cfb2b
Łukasz Mrozek <morskk@gmail.com>
2017-08-19 16:37:01 +0200

    Added CSharp6 project, just to see how C#6 interfaces with C#7 features (refs specifically)

---
+++ CSharp6/UseCSharp7RefReturnMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CSharp7;
+
+namespace CSharp6
+{
+    public static class UseCSharp7RefReturnMethod
+    {
+        public static int Method()
+        {
+            var variable = 1;
+            var result = RefReturns.Method(ref variable);
+            return result;
+        }
+    }
+}
