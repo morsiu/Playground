7dad3ca7d4c1686a0faa358e364f4bce5f87f622
Łukasz Mrozek <morskk@gmail.com>
2017-08-19 16:36:22 +0200

    Added SelfType, where I try to write code which preserves type of manipulated value while not being tied to the type

---
+++ SelfType/GenericManipulation.cs
@@ -0,0 +1,32 @@
+﻿namespace SelfType
+{
+    public static class GenericManipulation
+    {
+        private interface IInterface<TSelf>
+            where TSelf : IInterface<TSelf>
+        {
+            TSelf DoA();
+
+            TSelf DoB();
+        }
+
+        private static T Manipulation<T>(T input)
+            where T : IInterface<T>
+        {
+            return input.DoA().DoB();
+        }
+
+        private static void Consumer()
+        {
+            var x = new Implementation();
+            var result = Manipulation(x);
+        }
+
+        private sealed class Implementation : IInterface<Implementation>
+        {
+            public Implementation DoA() => this;
+
+            public Implementation DoB() => this;
+        }
+    }
+}
---
+++ SelfType/NonGenericManipulation.cs
@@ -0,0 +1,23 @@
+﻿namespace SelfType
+{
+    public static class NonGenericManipulation
+    {
+        private static void Consumer()
+        {
+            var x = new Implementation();
+            var result = x.Manipulation();
+        }
+
+        private sealed class Implementation
+        {
+            public Implementation Manipulation()
+            {
+                return DoA().DoB();
+            }
+
+            Implementation DoA() => this;
+
+            Implementation DoB() => this;
+        }
+    }
+}
