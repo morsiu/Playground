ab4f39e542f5fe912509316b0c7c971ef27b02f0
Łukasz Mrozek <morskk@gmail.com>
2017-08-19 16:34:14 +0200

    Added code using ref local and ref return to see how it looks in IL. Renamed file to match class name.

---
+++ CSharp7/RefLocal.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CSharp7
+{
+    public static class RefLocal
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static int Method(ref int parameter)
+        {
+            var variable = parameter + 17;
+            return Method2(ref variable);
+        }
+
+        public static int Method2(ref int parameter)
+        {
+            return Method(ref parameter);
+        }
+    }
+}
---
+++ CSharp7/RefReturns.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp7
+{
+    public static class RefReturns
+    {
+        public static ref int Method(ref int input)
+        {
+            return ref input;
+        }
+    }
+}
